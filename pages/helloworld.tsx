1bce58cd009b0cf0c4413c30c3786ddcb81ccabd
unknown <nguyentrungdung12a@gmail.com>
2022-11-16 19:21:36 +0700

    init

---
+++ pages/helloworld.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const helloworld = (props: Props) => {
+    return (
+        <div>
+            This is Hello World Page
+        </div>
+    )
+}
+
+export default helloworld
